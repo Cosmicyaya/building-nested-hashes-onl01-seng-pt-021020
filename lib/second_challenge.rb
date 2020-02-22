3df6f22024ec7ef735067c43618e97fa0e5bd193
Ryan Hollins <ballbaroi@gmail.com>
2020-02-22 08:20:25 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,15 +5,13 @@ def second_challenge
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-      #your key/value pairs here!
-      }, 
+      },
     :capulet => {
-      :patriach => {}
-      :matriarch => {}
-      :hero => {}
-      :hero_friends => []
-      #your key/value pairs here!
-      
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+      }}
 
   
 end
